edcd7f1c246f0a97a9ab8430d4b93616353bd70a
krishna1500 <krishna16151@gmail.com>
2023-08-22 01:16:59 +0530

    Active links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 const Nav = () => {
   return (
     <div>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/about'>About</NavLink>
     </div>
   )
 }

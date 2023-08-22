db488f40e0ce278cd51e0bcf2f0e407b902b5674
krishna1500 <krishna16151@gmail.com>
2023-08-22 23:54:36 +0530

    No Match Router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './Components/Home';
 import About from './Components/About';
 import Nav from './Components/Nav';
-// import OrderSummary from './Components/OrderSummary';
+import OrderSummary from './Components/OrderSummary';
+import NoMatch from './Components/NoMatch';
 function App() {
   return (
     <div className="App">
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element= {<Home/>}/>
         <Route path='about' element= {<About/>}/>
-        {/* <Route path='order-summary' element={<OrderSummary/>}/> */}
+        <Route path='order-summary' element={<OrderSummary/>}/>
+        <Route path='*' element={ <NoMatch/> }/>
       </Routes>
       </>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const Home = () => {
-  // const navigate= useNavigate()
+  const navigate= useNavigate()
   return (
     <div>
       <h1>Home Page</h1>
-      {/* <button onClick={()=> navigate('order-summary')}>Place Order</button> */}
+      <button onClick={()=> navigate('order-summary')}>Place Order</button>
     </div>
   )
 }
